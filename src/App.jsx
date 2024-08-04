4c0e9f679fc203b9bdc9fbabbde3c8cae0c89d64
Viktoriia <freshitpte@gmail.com>
2024-08-04 20:52:28 +0300

    Add theme styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
+import classNames from "classnames";
+import { connect } from "react-redux";
 import Counter from "./components/Counter";
 import ThemeSwitcher from "./components/ThemeSwitcher";
+import styles from "./App.module.css";
+import CONSTANTS from "./constants";
 
-function App() {
+const { LIGHT, DARK, BLUE } = CONSTANTS.THEMES;
+
+function App({ theme }) {
+  const appClassName = classNames(styles.container, {
+    [styles.light]: theme === LIGHT,
+    [styles.dark]: theme === DARK,
+    [styles.blue]: theme === BLUE,
+  });
+
   return (
-    <>
+    <div className={appClassName}>
       <Counter />
       <ThemeSwitcher />
-    </>
+    </div>
   );
 }
 
-export default App;
+const mapStateToProps = ({ themeData }) => themeData;
+
+export default connect(mapStateToProps)(App);
